2e9c8ffb02fffd4cf3441af97e2e9d7b26f12472
Ryan Middleton <ryan@pop-os.localdomain>
2021-11-19 01:29:05 +0000

    Made small update to dividend output

--- tickerbot.py
+++ tickerbot.py
@@ -14,9 +14,11 @@ async def on_message(message):
 
     if message.content.startswith('!'):
         ticker = (message.content.upper().lstrip("!"))
-        stock = yf.Ticker(ticker)
-        data = stock.info
-        output = ("Name: " + data['longName'] + "\n" + "Ticker: " + data['symbol'] + "\n" + "Current Price: " + str(data['currentPrice']) + data['currency'] + "\n" + "Dividend: " + str(data['dividendRate']) + data['currency'])
+        data = yf.Ticker(ticker).info
+        if str(data['dividendRate']) == "None":
+            output = ("Name: " + data['longName'] + "\n" + "Ticker: " + data['symbol'] + "\n" + "Current Price: " + str(data['currentPrice']) + data['currency'] + "\n" + "Dividend: " + str(data['dividendRate']))
+        else:
+            output = ("Name: " + data['longName'] + "\n" + "Ticker: " + data['symbol'] + "\n" + "Current Price: " + str(data['currentPrice']) + data['currency'] + "\n" + "Dividend: " + str(data['dividendRate']) + data['currency'])
         await message.channel.send(output)
 
-client.run('')	# Discord bot API key goes here
+client.run('OTExMDA2MjEzNDM5OTAxNzQ3.YZbGpg.aUp2l7bq-LneubkIJihwqHxtS1I')	# Discord bot API key goes here
